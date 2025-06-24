44be67a12f6e180d4dd74a7bc613cfb4fa15c622
maru-x <m_goshono@yahoo.co.jp>
2025-06-24 22:55:41 +0900

    mcb_timer_cancel関数の宣言を修正し、C++との互換性を確保。関数の説明を追加。

--- src/behaviors/behavior_mouse_click_back.h
+++ src/behaviors/behavior_mouse_click_back.h
@@ -1,2 +1,14 @@
-void mcb_timer_cancel()
+#pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief 外部から遅延ワーク（自動マウスレイヤー解除タイマー）をキャンセル
+ */
+void mcb_timer_cancel(void);
+
+#ifdef __cplusplus
+}
+#endif
